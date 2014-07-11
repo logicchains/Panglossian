c4e78cf8296e726a740f8458bffbcfc14a1d4804
Jonathan <jonathan.t.barnard@gmail.com>
2014-07-11 23:39:19 +1000

    began implementing command parser

--- CmdParser.hs
+++ CmdParser.hs
@@ -1,11 +1,16 @@
 module Panglossian.CmdParser (parseCommand) where
 
 import Control.Concurrent.STM
+import Data.Either
+import Data.List
 import Data.Int
 
 data Token = NumToken Int32 | StrToken String
+           deriving Show
 
 type Command = (String, [Token])
+
+commands = ["listen"]
 
 parseInt :: String -> Maybe Int32
 parseInt str = case (reads str :: [(Int32, String)]) of
@@ -13,13 +18,17 @@ parseInt str = case (reads str :: [(Int32, String)]) of
     [(num, _)] -> Just num
 
 expectToken :: Token -> String -> Either String Token
-expectToken (StrToken _) str = StrToken str
+expectToken (StrToken _) str = Right $ StrToken str
 expectToken (NumToken _) str = case (parseInt str) of
-                                  Just n -> NumToken n
-                                  Nothing -> "Error: "++str++" is not an int."
+                                  Just n -> Right $ NumToken n
+                                  Nothing -> Left ("Error: "++str++" is not an int.")
 
-expectTokens :: [Token] -> [String] -> Maybe [Token]  
-expectTokens expected actual = zipWith expectToken expected actual
+expectTokens :: [Token] -> [String] -> ([String], [Token])
+expectTokens expected actual = partitionEithers $ zipWith expectToken expected actual
 
-parseCommand :: [String] -> Command
-parseCommand str = ("", [NumToken 10])
+sendCommand :: Command -> TChan Command -> IO()
+sendCommand cmd chan = atomically $ writeTChan chan cmd
+
+parseCommand :: [String] -> Either String Command
+parseCommand [] = Left "Error: no command entered\n" 
+parseCommand [x] = if elem x commands then Right (x, []) else Left ("Error: invalid command: " ++ x)
